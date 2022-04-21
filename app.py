7b47aae831db9ba9499a29ef56445dcf77d61e92
joseantoniocgonzalez <er.joselin@gmail.com>
2022-04-21 12:51:57 +0200

    mas modificaciones

--- app.py
+++ app.py
@@ -1,41 +1,31 @@
-#Importamos lo que vamos a necesitar de flask
-from flask import Flask, render_template,abort
-# Importamos la libreria os del sistema para utilizar environ
-import os 
-# La aplicación nos va permitir mostrar la información del fichero books.json
-# Por tanto, importamos json para leer el fichero 
-import json 
-# Definimos la variable app por Flask
-app = Flask(__name__)
-# Leemos el fichero y lo añadimos a una variable 
-with open ("books.json") as fichero:
-    info = json.load(fichero)
+from flask import Flask, render_template, abort
+import json
+import os
 
-# Definimos la ruta principal de la página de incio 
 
-@app.route('/',methods=["GET","POST"])
+app=Flask(__name__)
+
+with open("books.json") as libros:
+    datos=json.load(libros)
+
+@app.route('/')
 def inicio():
-	return render_template("inicio.html", lista_libros=info)
+    nombre='jose antonio canalo '
+    return render_template('inicio.html', libros=datos, nombre=nombre)
 
-# Definimos la ruta secundaria donde va estar la información del libro al que accedamos. La función recibe el codigo isbn
-@app.route('/libro/<isbn>',methods=["GET","POST"])
-def info_libros(isbn):
-    for elem in info:
-        if "isbn" in elem.keys() and isbn == elem["isbn"]:
-        	return render_template("detalle_libro.html", detalle_libro=elem)
-    abort(404)
 
-# Definimos la tercera ruta de las categorías.
-@app.route('/categoria/<categoria>',methods=["GET","POST"])
+@app.route('/libro/<isbn>')
+def libro(isbn):
+    for lib in datos:
+        if "isbn" in lib.keys() and isbn == lib["isbn"]:
+            return render_template('libros.html', libro=lib)
+
+    return abort(404) 
+@app.route('/categoria/<categoria>')
 def categoria(categoria):
-    for category in info:
-        if "categories" in category.keys() and categoria in category["categories"]:
-            return render_template("categoria.html", lista_libros=info, categoria=categoria)
-
-
-#Probar en el entorno de desarrollo
-#app.run(debug=True)
-
-
-
-
+    for i in datos:
+        if "categories" in i.keys() and categoria in i["categories"]:
+            return render_template("categoria.html",libros=datos,categoria=categoria)
+    abort(404)
+    
+app.run('0.0.0.0', debug=True)
