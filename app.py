22f8f6a1b69dff71baff361b1beaea5d003ee0e6
joseantoniocgonzalez <er.joselin@gmail.com>
2022-04-21 12:00:53 +0200

    arreglando app.py

--- app.py
+++ app.py
@@ -1,33 +1,23 @@
 from flask import Flask, render_template, abort
 import json
 import os
-
-
 app=Flask(__name__)
-
 with open("books.json") as libros:
     datos=json.load(libros)
-
 @app.route('/')
 def inicio():
-    nombre='Omar Elhani Botkala'
+    nombre='Jose antonio Canalo'
     return render_template('inicio.html', libros=datos, nombre=nombre)
-
-
 @app.route('/libro/<isbn>')
 def libro(isbn):
-    for lib in datos:
-        if "isbn" in lib.keys() and isbn == lib["isbn"]:
-            return render_template('libros.html', libro=lib)
+    for l in datos:
+        if "isbn" in l.keys() and isbn == l["isbn"]:
+            return render_template('libros.html', libro=l) 
 
-    return abort(404) 
+    return abort(404)
+
 @app.route('/categoria/<categoria>')
 def categoria(categoria):
-    for i in datos:
-        if "categories" in i.keys() and categoria in i["categories"]:
-            return render_template("categoria.html",libros=datos,categoria=categoria)
-    abort(404)
+            return render_template('categoria.html', libros=datos, categoria=categoria)
 
-
-port=os.environ["PORT"]
-app.run('0.0.0.0', int(port), debug=True)
+app.run('0.0.0.0',debug=False)
